96efe777bb13f7d64d6e5882f8df5c7aad15034b
Nick Greenwood <nick.greenwood.zen@hotmail.com>
2021-08-02 09:44:37 +0100

    Added ClockInMirror

---
+++ Katas/ClockInMirrorTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using FluentAssertions;
+using Xunit;
+
+namespace Katas
+{
+    public class ClockInMirrorTests
+    {
+        [Theory]
+        [InlineData("12:22", "11:38")]
+        [InlineData("05:25", "06:35")]
+        [InlineData("01:50", "10:10")]
+        [InlineData("11:58", "12:02")]
+        [InlineData("12:01", "11:59")]
+        public void CheckTheTime(string currentTime, string expectedTime)
+            => new ClockInMirror().WhatIsTheTime(currentTime).Should().Be(expectedTime);
+    }
+
+    public class ClockInMirror
+    {
+        private static readonly DateTime Midnight = new(2021, 1, 1, 12, 0, 0);
+
+        public string WhatIsTheTime(string currentTime)
+        {
+            var time = DateTime.ParseExact(currentTime, @"hh\:mm", CultureInfo.CurrentCulture);
+
+            return time.Add((Midnight - time) * 2).ToString(@"hh\:mm");
+        }
+    }
+}
