590e13e7a9448d27181e078e6016f7704aa07651
Nick Greenwood <nick.greenwood.zen@hotmail.com>
2021-08-02 08:19:05 +0100

    Added OddEvenWordSplitter and new version of FizzBuzz

---
+++ Katas/FizzBuzzBoomTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Katas
+{
+    public class FizzBuzzBoomTests
+    {
+        [Theory]
+        [InlineData(1, "1")]
+        [InlineData(2, "2")]
+        [InlineData(3, "Fizz")]
+        [InlineData(5, "Buzz")]
+        [InlineData(6, "Fizz")]
+        [InlineData(10, "Boom")]
+        [InlineData(30, "Boom")]
+        [InlineData(15, "FizzBuzz")]
+        public void WhenSpeakingANumberCheckTheReply(int number, string answer)
+            => Assert.Equal(answer, new FizzBuzzBoomGame().Speak(number));
+    }
+
+    public class FizzBuzzBoomGame
+    {
+        private readonly IEnumerable<(int Number, string Word)> _overrideRules = new[]
+        {
+            (10, "Boom")
+        };
+
+        private readonly IEnumerable<(int Number, string Word)> _rules = new[]
+        {
+            (3, "Fizz"),
+            (5, "Buzz")
+        };
+
+        public string Speak(int number)
+        {
+            return _overrideRules
+                .Where(rule => number % rule.Number == 0)
+                .Select(rule => rule.Word)
+                .DefaultIfEmpty(string.Join("", _rules
+                    .Where(rule => number % rule.Number == 0)
+                    .Select(rule => rule.Word)
+                    .DefaultIfEmpty(number.ToString())))
+                .First();
+        }
+    }
+}
---
+++ Katas/OddEvenWordSplitterTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Katas
+{
+    public class OddEvenWordSplitterTests
+    {
+        [Theory]
+        [InlineData("Nick", "Nc ik")]
+        [InlineData("Nicholas", "Ncoa ihls")]
+        [InlineData("N", "N")]
+        public void Test(string word, string answer)
+            => Assert.Equal(answer, new OddEvenWordSplitter().Split(word));
+    }
+
+    public class OddEvenWordSplitter
+    {
+        public string Split(string word)
+        {
+            return string.Join(" ", word
+                .ToCharArray()
+                .Select((character, i) => new { character, isEven = i % 2 == 0 })
+                .GroupBy(character => character.isEven)
+                .Select(grouping => string.Join("", grouping.Select(x => x.character))));
+        }
+    }
+}
