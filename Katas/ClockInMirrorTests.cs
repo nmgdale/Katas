227778e0a77943d0966e67cf8e1dddd931275a82
Nick Greenwood <nick.greenwood.zen@hotmail.com>
2021-08-02 12:42:27 +0100

    Corrected after rules misunderstood

--- Katas/ClockInMirrorTests.cs
+++ Katas/ClockInMirrorTests.cs
@@ -15,13 +15,13 @@ namespace Katas
         [InlineData("01:50", "10:10")]
         [InlineData("11:58", "12:02")]
         [InlineData("12:01", "11:59")]
-        [InlineData("12:30", "12:30")]
+        [InlineData("12:30", "11:30")]
         [InlineData("06:00", "06:00")]
-        [InlineData("06:30", "06:30")]
-        [InlineData("06:15", "06:45")]
-        [InlineData("06:45", "06:15")]
-        [InlineData("12:15", "12:45")]
-        [InlineData("12:45", "12:15")]
+        [InlineData("06:30", "05:30")]
+        [InlineData("06:15", "05:45")]
+        [InlineData("06:45", "05:15")]
+        [InlineData("12:15", "11:45")]
+        [InlineData("12:45", "11:15")]
         public void CheckTheTime(string currentTime, string expectedTime)
             => new ClockInMirror().WhatIsTheTime(currentTime).Should().Be(expectedTime);
     }
@@ -30,19 +30,11 @@ namespace Katas
     {
         private static readonly DateTime Midnight = new(2021, 1, 1, 12, 0, 0);
 
-        private readonly List<int> _mirroredHours = new() { 0, 6 };
-        private readonly List<int> _mirroredMinutes = new() { 0, 15, 30, 45 };
-
         public string WhatIsTheTime(string currentTime)
         {
             var time = DateTime.ParseExact(currentTime, @"hh\:mm", CultureInfo.CurrentCulture);
 
-            var mirroredTime = time.Add((Midnight - time) * 2);
-
-            if (_mirroredHours.Contains(time.Hour) && _mirroredMinutes.Contains(time.Minute))
-                mirroredTime = new DateTime(2021, 1, 1, time.Hour, mirroredTime.Minute, 0);
-
-            return mirroredTime.ToString(@"hh\:mm");
+            return time.Add((Midnight - time) * 2).ToString(@"hh\:mm");
         }
     }
 }
