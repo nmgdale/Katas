b831ad76dfd5b2d514410391979421620fa8b1c4
Nick Greenwood <nick.greenwood.zen@hotmail.com>
2021-10-05 08:31:49 +0100

    Refactor

--- Katas/LibraryKata/Repositories/LibraryDatabase.cs
+++ Katas/LibraryKata/Repositories/LibraryDatabase.cs
@@ -8,45 +8,27 @@ namespace Katas.LibraryKata.Repositories
     public class LibraryDatabase : ILibraryRepository
     {
         public IEnumerable<string> GetMembersBooks(string memberId)
-        {
-            using var connection = new SqliteConnection("Data Source=Library.sqlite");
-
-            return connection.Query<string>("SELECT BookId FROM MemberBooks WHERE MemberId = @Id", new { Id = memberId });
-        }
+            => CreateDatabase().Query<string>("SELECT BookId FROM MemberBooks WHERE MemberId = @Id", new { Id = memberId });
 
         public void BookOut(string memberId, string bookId)
-        {
-            using var connection = new SqliteConnection("Data Source=Library.sqlite");
-
-            connection.Execute("INSERT INTO MemberBooks (MemberId, BookId) VALUES (@memberId, @bookId)", new { memberId, bookId});
-        }
+            => CreateDatabase().Execute("INSERT INTO MemberBooks (MemberId, BookId) VALUES (@memberId, @bookId)", new { memberId, bookId});
 
         public void Return(string memberId, string bookId)
-        {
-            using var connection = new SqliteConnection("Data Source=Library.sqlite");
-
-            connection.Execute("DELETE FROM MemberBooks WHERE MemberId = @memberId AND BookId = @bookId", new { memberId, bookId });
-        }
+            => CreateDatabase().Execute("DELETE FROM MemberBooks WHERE MemberId = @memberId AND BookId = @bookId", new { memberId, bookId });
 
         public Book GetBook(string id)
-        {
-            using var connection = new SqliteConnection("Data Source=Library.sqlite");
-
-            return connection.QueryFirstOrDefault<Book>("SELECT * FROM Books WHERE Id = @id", new { id });
-        }
+            => CreateDatabase().QueryFirstOrDefault<Book>("SELECT * FROM Books WHERE Id = @id", new { id });
 
         public Member GetMember(string id)
-        {
-            using var connection = new SqliteConnection("Data Source=Library.sqlite");
-
-            return connection.QueryFirstOrDefault<Member>("SELECT * FROM Members WHERE Id = @id", new { id });
-        }
+            => CreateDatabase().QueryFirstOrDefault<Member>("SELECT * FROM Members WHERE Id = @id", new { id });
 
         public string OwnedBy(string bookId)
+            => CreateDatabase().QueryFirst<string>("SELECT MemberId FROM MemberBooks mb WHERE mb.BookId = @Id", new { Id = bookId });
+
+        private static SqliteConnection CreateDatabase()
         {
             using var connection = new SqliteConnection("Data Source=Library.sqlite");
-
-            return connection.QueryFirst<string>("SELECT MemberId FROM MemberBooks mb WHERE mb.BookId = @Id", new { Id = bookId });
+            return connection;
         }
     }
 }
