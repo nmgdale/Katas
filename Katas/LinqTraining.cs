79433b7ccd047af34832b528c6356c09b203ad63
Nick Greenwood <nick.greenwood.zen@hotmail.com>
2021-10-11 09:34:29 +0100

    Added linq training file

---
+++ Katas/LinqTraining.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Bogus;
+using Xunit;
+
+namespace Katas
+{
+    public class LinqTraining
+    {
+        private static readonly Faker Faker = new();
+
+        private static readonly IEnumerable<Order> Orders = Enumerable.Range(0, Faker.Random.Number(100, 200))
+            .Select(i => new Order
+            {
+                Items = Enumerable.Range(0, Faker.Random.Number(1, 10))
+                    .Select(y => new Item
+                    {
+                        Type = Faker.PickRandom("Hardware", "Broadband", "Voice", "Domain"),
+                        Price = new Money(Faker.Random.Number(0, 100))
+                    })
+            });
+
+        //Select()
+        //SelectMany()
+        //Any() All()
+        //Aggregate()
+        //GroupBy()
+        //Skip() Take()
+        //First() Last() Single()
+
+        [Fact]
+        public void Test()
+        {
+        }
+    }
+
+    public class Order
+    {
+        public IEnumerable<Item> Items = new List<Item>();
+    }
+
+    public class Item
+    {
+        public string Type;
+        public Money Price;
+    }
+}
